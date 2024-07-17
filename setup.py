d7dce5527ec9082e4472ecf3d34faae226df50d7
defer logic <defer_logic@proton.me>
2024-07-17 21:22:32 +0300

    use version from setuptools

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import find_packages, setup
 
 setup(
     name="eth-packages",
-    version="0.1.1",
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
     packages=find_packages(
         include=[
             "packages/eth_rpc*",
