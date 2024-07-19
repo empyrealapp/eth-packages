a6e60a4fa9c13ffa081aa0fc2a651d4a5a10ab29
defer logic <defer_logic@proton.me>
2024-07-19 15:58:51 +0300

    add missing peapods router

---
+++ packages/eth_typeshed/src/eth_typeshed/peapods/router.py
@@ -0,0 +1,35 @@
+from eth_rpc.contract import ContractFunc
+from eth_rpc.types import METHOD, NoArgs, primitives
+from eth_typeshed._base import ProtocolBase
+from eth_typing import HexAddress
+from pydantic import BaseModel
+
+
+class FunctionType:
+    Bond = 0
+    Debond = 1
+    Approve = 2
+    SwapV2 = 3
+    SwapV3 = 4
+
+
+class FunctionArg(BaseModel):
+    type: primitives.uint8
+    data: primitives.bytes
+
+
+class PeapodsRouter(ProtocolBase):
+    route: ContractFunc[
+        tuple[
+            primitives.uint256,
+            HexAddress,
+            HexAddress,
+            list[tuple[primitives.uint8, primitives.bytes]],
+        ],
+        primitives.uint256,
+    ] = METHOD
+
+    ping: ContractFunc[
+        NoArgs,
+        primitives.uint256,
+    ] = METHOD
