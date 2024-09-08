da576e5594a5ac017a21fe53731e61bb59ec3156
johnny-emp <164106913+johnny-emp@users.noreply.github.com>
2024-09-07 22:22:25 -0400

    add arb sepolia (#39)
    * add arb sepolia
    
    * format

--- packages/eth_rpc/src/eth_rpc/networks/arbitrum.py
+++ packages/eth_rpc/src/eth_rpc/networks/arbitrum.py
@@ -22,3 +22,22 @@ class Arbitrum(Network):
     )
     alchemy_str: ClassVar[str | None] = "arb-mainnet"
     apprx_block_time: ClassVar[float] = 0.26
+
+
+class ArbitrumSepolia(Network):
+    chain_id: ClassVar[int] = 421614
+    name: ClassVar[str] = "ArbitrumSepolia"
+    native_currency: ClassVar[str] = "ETH"
+    rpc: ClassVar[Rpcs] = Rpcs(
+        default=RpcUrl(
+            http=Url("https://arbitrum-sepolia.publicnode.com"),
+            wss=None,
+        )
+    )
+    block_explorer: ClassVar[BlockExplorer] = BlockExplorer(
+        name="Arbiscan",
+        url="https://sepolia.arbiscan.io",
+        api_url="https://api-sepolia.arbiscan.io/api",
+    )
+    alchemy_str: ClassVar[str | None] = "arb-sepolia"
+    apprx_block_time: ClassVar[float] = 0.26
