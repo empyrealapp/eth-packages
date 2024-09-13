b1d09dae73e6466688b1a19fa70ee2b15f7fd741
johnny-emp <johnny@empyrealsdk.com>
2024-09-13 13:31:29 -0400

    improved struct parsing

--- packages/eth_rpc/tests/test_func_signature.py
+++ packages/eth_rpc/tests/test_func_signature.py
@@ -1,6 +1,7 @@
 from typing import Annotated
 
 import pytest
+from eth_abi import encode
 from eth_rpc import FuncSignature
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -12,7 +13,6 @@ from eth_rpc.types import NoArgs, Name, Struct, primitives
 from eth_rpc.types import Name, NoArgs, Struct, primitives
 >>>>>>> a3577d8 (format)
 from eth_rpc.utils.types import transform_primitive
-from eth_typing import HexStr
 from pydantic import BaseModel
 
 
@@ -125,21 +125,27 @@ def test_func_encode() -> None:
 
 @pytest.mark.unit
 def test_func_decode_struct_list() -> None:
+    class MyNestedStruct(Struct):
+        a: primitives.uint256
+        b: primitives.uint256
+
     class MyStruct(Struct):
         x: primitives.uint256
         y: bool
+        z: list[MyNestedStruct]
 
     func = FuncSignature[
         NoArgs,
         list[MyStruct],
     ](name="func")
 
-    result = func.decode_result(
-        HexStr(
-            "0x00000000000000000000000000000000000000000000000000000000000000"
-            "20000000000000000000000000000000000000000000000000000000000000000"
-            "1000000000000000000000000000000000000000000000000000000000000000"
-            "10000000000000000000000000000000000000000000000000000000000000001"
-        )
+    my_struct = MyStruct(x=1, y=True, z=[MyNestedStruct(a=1, b=2)])
+    encoded_bytes = encode(
+        ['(uint256,bool,(uint256,uint256)[])[]'],
+        [[(1, True, [(1, 2)])]]
     )
-    assert result[0] == MyStruct(x=1, y=True)
+
+    result = func.decode_result(
+        encoded_bytes.hex()
+    )
+    assert result[0] == my_struct
