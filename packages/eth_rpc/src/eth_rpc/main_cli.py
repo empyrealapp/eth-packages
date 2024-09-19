eaaf23e2d3b9a72eadc6bdc94c0147277b1472ef
johnny-emp <johnny@empyrealsdk.com>
2024-09-19 11:13:50 -0400

    codegen

--- packages/eth_rpc/src/eth_rpc/main_cli.py
+++ packages/eth_rpc/src/eth_rpc/main_cli.py
@@ -1,7 +1,9 @@
 import json
 
 import click
+from eth_typing import HexAddress
 
+from .block_explorer import get_abi
 from .codegen import codegen as codegen_cmd
 
 
@@ -10,22 +12,41 @@ def cli():
     pass
 
 
+@click.group()
+def codegen():
+    pass
+
+
 @click.command()
 @click.argument("input_file")
-@click.argument("output_file")
-@click.option("-c", "--class-name")
-def codegen(input_file, output_file, class_name: str | None = None):
-    if not class_name:
-        class_name = "AnonContract"
+@click.option("--output", "-o", default="abi.py")
+@click.option("--contract-name", "-c", default="AnonContract")
+def load(input_file, output_file, contract_name: str):
     with open(input_file, "r") as f:
         raw_abi = f.read()
 
     abi = json.loads(raw_abi)
     with open(output_file, "w") as f:
-        f.write(codegen_cmd(abi, class_name))
+        f.write(codegen_cmd(abi, contract_name))
+
+
+@click.command()
+@click.option("--network", "-n", default="ethereum")
+@click.option("--address", "-a", required=True)
+@click.option("--output", "-o", default="abi.py")
+@click.option("--api_key", "-a", required=True)
+@click.option("--contract-name", "-c", default="AnonContract")
+def explorer(network: str, address: HexAddress, output: str, api_key, contract_name: str):
+    if network.lower() != "ethereum":
+        click.echo("Network not yet supported.  Coming soon!")
+    abi = get_abi(address, api_key)
+    with open(output, "w") as f:
+        f.write(codegen_cmd(abi, contract_name))
 
 
 cli.add_command(codegen)
+codegen.add_command(load)
+codegen.add_command(explorer)
 
 if __name__ == "__main__":
     cli()
