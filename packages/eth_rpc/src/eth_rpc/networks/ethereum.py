21fb89c4813ac079c6244ce64259e3c2fe19d07e
johnny-emp <johnny@empyrealsdk.com>
2024-12-17 17:09:30 -0500

    add sepolia to networks

--- packages/eth_rpc/src/eth_rpc/networks/ethereum.py
+++ packages/eth_rpc/src/eth_rpc/networks/ethereum.py
@@ -21,3 +21,22 @@ class Ethereum(Network):
     )
     alchemy_str: ClassVar[str | None] = "eth-mainnet"
     apprx_block_time: ClassVar[float] = 12.05
+
+
+class Sepolia(Network):
+    chain_id: ClassVar[int] = 11155111
+    name: ClassVar[str] = "Ethereum Sepolia"
+    native_currency: ClassVar[str] = "ETH"
+    rpc: ClassVar[Rpcs] = Rpcs(
+        default=RpcUrl(
+            http=AnyHttpUrl("https://sepolia.gateway.tenderly.co"),
+            wss=AnyWebsocketUrl("wss://ethereum-sepolia-rpc.publicnode.com"),
+        )
+    )
+    block_explorer: ClassVar[BlockExplorer] = BlockExplorer(
+        name="Etherscan Sepolia",
+        url="https://sepolia.etherscan.io",
+        api_url="https://api-sepolia.etherscan.io/api",
+    )
+    alchemy_str: ClassVar[str | None] = "eth-sepolia"
+    apprx_block_time: ClassVar[float] = 12.05
